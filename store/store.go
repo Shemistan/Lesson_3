95444fb19c432cb221a105901180440db18622f9
Shamistan <shemistan1@gmail.com>
2023-08-01 19:01:21 +0300

    initial commit

---
+++ store/store.go
@@ -0,0 +1,19 @@
+package store
+
+func SaveText(req string) bool {
+	// code
+
+	return true
+}
+
+func SaveInt(req string) bool {
+	// code
+
+	return true
+}
+
+func daveText(req string) bool {
+	// code
+
+	return true
+}
