b39e49e4e4342dc9cd3ab9b90abbd9e4b45d0dcb
PAVEL ZEMSKOV <pzemskov@gmail.com>
2023-08-13 01:36:37 +0500

    lesson 3 function_add

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/Zzema/Lesson_3/store"
 	//"github.com/Zzema/lesson_3/store"
 )
 
@@ -15,14 +16,13 @@ const (
 
 func main() {
 	var command string
-	var flag_auth bool //признак идентификации пользователя
+	var flagAuth, flagReg bool //признак идентификации пользователя
 	userList := []string{"user1_password1", "user2_password2"}
 	productList := make([]string, 0, 10)
 
 	//_ = productList
 	for command != exit {
-		fmt.Println("Введите команду, одну из exit/auth/reg/add_product/order") // Сделать красивый вывод, вывести список команд на этом шаге
-		//store.StartPrint()
+		store.StartPrint()
 		fmt.Scan(&command)
 
 		switch command {
@@ -30,53 +30,16 @@ func main() {
 			fmt.Println("exiting")
 			break
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_password")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
+			flagReg = store.Registration(&userList)
 
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
-
-			fmt.Println("Привет")
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_password")
-			fmt.Scan(&command)
-
-			for _, v := range userList {
-				if v == command {
-					fmt.Println("Добро пожаловать в магазин")
-					flag_auth = true
-				}
-			}
-			if flag_auth != true {
-				fmt.Println("Вы не зарегистрированны или неверный пароль")
-			}
+			flagAuth = store.Auth(flagReg, &userList)
 		case add_product:
-			//если авторизованы - добавляем товары
-			if flag_auth == true {
-				fmt.Println("Какой товар хотите добавить?")
-				fmt.Scan(&command)
-				productList = append(productList, command)
-				fmt.Println("Следующие товары в корзине", productList)
-			} else {
-				fmt.Println("Вы не авторизованы")
-			}
-		case order:
-			//если авторизованы - добавляем товары
-			if flag_auth == true && len(productList) > 0 {
-				fmt.Println("Товары из корзины заказаны,", productList, "корзина очищена")
-				productList = nil
-			} else if flag_auth == false {
-				fmt.Println("Вы не авторизованы")
-			} else {
-				fmt.Println("Нет товаров в корзине, добавьте товары через add_product")
-			}
+			store.ProductAdd(flagAuth, &productList)
 
+		case order:
+			store.Order(flagAuth, &productList)
 		}
 	}
 
 }
-
-// Реализовать следующие АПИ
-// add_product - добавить продукт который вводиться с консоли в productList
-// order - выводит сообщение что вы купили и очищает корзину
--- store/store.go
+++ store/store.go
@@ -2,24 +2,68 @@ package store
 
 import "fmt"
 
-func SaveText(req string) bool {
-	// code
-
-	return true
-}
-
-func SaveInt(req string) bool {
-	// code
-
-	return true
-}
-
-func daveText(req string) bool {
-	// code
-
-	return true
-}
-
-func startPrint() {
+func StartPrint() {
 	fmt.Println("Введите команду, одну из exit/auth/reg/add_product/order")
+}
+
+// добавляем нового пользователя
+func Registration(userList *[]string) bool {
+	var command string
+	fmt.Println("Введите логин и пароль в таком виде login_password")
+	fmt.Scan(&command)
+	*userList = append(*userList, command)
+
+	message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+	fmt.Println(message)
+	return true
+}
+
+// Аутентификация пользователя, если не удалась - возвращаем неуспешный флаг
+func Auth(flagReg bool, userList *[]string) bool {
+	var flagAuth bool
+	var command string
+	if flagReg == true {
+		fmt.Println("Добро пожаловать в магазин")
+		flagAuth = true
+	} else {
+		fmt.Println("Введите логин и пароль в таком виде login_password")
+		fmt.Scan(&command)
+
+		for _, v := range *userList {
+			if v == command {
+				fmt.Println("Добро пожаловать в магазин")
+				flagAuth = true
+			}
+		}
+		if flagAuth != true {
+			fmt.Println("Вы не зарегистрированны или неверный пароль")
+			flagAuth = false
+		}
+	}
+	return flagAuth
+}
+
+// если авторизованы - добавляем товары
+func ProductAdd(flag_auth bool, productList *[]string) {
+	var command string
+	if flag_auth == true {
+		fmt.Println("Какой товар хотите добавить?")
+		fmt.Scan(&command)
+		*productList = append(*productList, command)
+		fmt.Println("Следующие товары в корзине", *productList)
+	} else {
+		fmt.Println("Вы не авторизованы")
+	}
+}
+
+// заказываем если есть товары и прошли авторизацию
+func Order(flag_auth bool, productList *[]string) {
+	if flag_auth == true && len(*productList) > 0 {
+		fmt.Println("Товары из корзины заказаны,", *productList, ", корзина очищена")
+		*productList = nil
+	} else if flag_auth == false {
+		fmt.Println("Вы не авторизованы")
+	} else {
+		fmt.Println("Нет товаров в корзине, добавьте товары через add_product")
+	}
 }
